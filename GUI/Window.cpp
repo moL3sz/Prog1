c80eb71b91292a92ac49d54adfc526da9fd70e91
moL3sz <molnarok2313@gmail.com>
2022-02-21 11:18:30 +0100

    FLTK GUI exit on x

--- GUI/Window.cpp
+++ GUI/Window.cpp
@@ -2,66 +2,74 @@
 #include "Graph.h"
 #include "GUI.h"
 
-namespace Graph_lib {
-
-Window::Window(int ww, int hh, const string& title)
-:Fl_Window(ww,hh,title.c_str()),w(ww),h(hh)
+namespace Graph_lib
 {
-	init();
-}
+	void exit_on_x(Fl_Widget *, void *)
+	{
+		exit(0);
+	}
 
-Window::Window(Point xy, int ww, int hh, const string& title)
-:Fl_Window(xy.x,xy.y,ww,hh,title.c_str()),w(ww),h(hh)
-{ 
-	init();
-}
+	Window::Window(int ww, int hh, const string &title)
+		: Fl_Window(ww, hh, title.c_str()), w(ww), h(hh)
+	{
+		init();
+	}
 
-void Window::init()
-{
-   resizable(this);
-   show();
-} 
+	Window::Window(Point xy, int ww, int hh, const string &title)
+		: Fl_Window(xy.x, xy.y, ww, hh, title.c_str()), w(ww), h(hh)
+	{
+		init();
+	}
 
-void Window::draw()
-{
-	Fl_Window::draw();
-	for (unsigned int i=0; i<shapes.size(); ++i) shapes[i]->draw();
-}
+	void Window::init()
+	{
+		callback(exit_on_x);
+		resizable(this);
+		show();
+	}
+	void Window::draw()
+	{
+		Fl_Window::draw();
+		for (unsigned int i = 0; i < shapes.size(); ++i)
+			shapes[i]->draw();
+	}
 
-void Window::attach(Widget& w)
-{
-	begin();
+	void Window::attach(Widget &w)
+	{
+		begin();
 		w.attach(*this);
-	end();
-}
+		end();
+	}
 
-void Window::detach(Widget& b)
-{
-	  b.hide();
-}
-
-void Window::attach(Shape& s)
-{
+	void Window::detach(Widget &b)
+	{
+		b.hide();
+	}
+	void Window::attach(Shape &s)
+	{
 		shapes.push_back(&s);
-}
-void Window::detach(Shape& s)
-{
-		for (unsigned int i = shapes.size(); 0<i; --i)
-			if (shapes[i-1]==&s)
-				shapes.erase(shapes.begin()+(i-1));
-}
+	}
+	void Window::detach(Shape &s)
+	{
+		for (unsigned int i = shapes.size(); 0 < i; --i)
+			if (shapes[i - 1] == &s)
+				shapes.erase(shapes.begin() + (i - 1));
+	}
 
-void Window::put_on_top(Shape& p) {
-	for (int i=0; i<shapes.size(); ++i) {
-		if (&p==shapes[i]) {
-			for (++i; i<shapes.size(); ++i)
-				shapes[i-1] = shapes[i];
-			shapes[shapes.size()-1] = &p;
-			return;
+	void Window::put_on_top(Shape &p)
+	{
+		for (int i = 0; i < shapes.size(); ++i)
+		{
+			if (&p == shapes[i])
+			{
+				for (++i; i < shapes.size(); ++i)
+					shapes[i - 1] = shapes[i];
+				shapes[shapes.size() - 1] = &p;
+				return;
+			}
 		}
 	}
+
+	int gui_main() { return Fl::run(); }
+
 }
-
-int gui_main() { return Fl::run(); }
-
-} 
